a0235ff805acaac7cb4c13adc36d87a8fe5502bd
senatorbot-team <30386153+senatorbot-team@users.noreply.github.com>
2017-07-24 01:20:37 +0300

    Add files via upload

---
+++ data/config.lua
@@ -0,0 +1,27 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "me",
+    "AAAA",
+    "replay"
+  },
+  info_text = "تاج راسكم حسوني \n @Dev_Hussein5_6",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    128897752,
+    319244649,
+    319244649,
+    435245104,
+    352465871
+  }
+}
+return _
+end
