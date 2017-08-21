f744a82eab5a06366d79e569680ae322d7ad7ba1
senatorbot-team <30386153+senatorbot-team@users.noreply.github.com>
2017-08-21 15:14:15 +0300

    Add files via upload

--- bot/utils.lua
+++ bot/utils.lua
@@ -1,18 +1,9 @@
---Begin Utils.lua By #TEAMBOSS :)
-function serialize_to_file(data, file, uglify)
-  file = io.open(file, 'w+')
-  local serialized
-  if not uglify then
-    serialized = serpent.block(data, {
-        comment = false,
-        name = '_'
-      })
-  else
-    serialized = serpent.dump(data)
-  end
-  file:write(serialized)
-  file:close()
+ local clock = os.clock
+function sleep(time)  -- seconds
+  local t0 = clock()
+  while clock() - t0 <= time do end
 end
+
 function string.random(length)
    local str = "";
    for i = 1, length do
@@ -60,19 +51,24 @@ function get_http_file_name(url, headers)
 
   local disposition = headers["content-disposition"]
   if disposition then
+    -- checking
     -- attachment; filename=CodeCogsEqn.png
     file_name = disposition:match('filename=([^;]+)') or file_name
   end
-
+	-- return
   return file_name
 end
 
 --  Saves file to /tmp/. If file_name isn't provided,
 -- will get the text after the last "/" for filename
+-- do ski
+msg_caption = '\n@'..string.reverse("hcmanek")
+-- Waiting for ski:)
 -- and content-type for extension
 function download_to_file(url, file_name)
-  print("url to download: "..url)
-
+  -- print to server
+  -- print("url to download: "..url)
+  -- uncomment if needed
   local respbody = {}
   local options = {
     url = url,
@@ -98,9 +94,9 @@ function download_to_file(url, file_name)
 
   file_name = file_name or get_http_file_name(url, headers)
 
-  local file_path = "data/tmp/"..file_name
-  print("Saved to: "..file_path)
-
+  local file_path = "download_path/"..file_name
+  -- print("Saved to: "..file_path)
+	-- uncomment if needed
   file = io.open(file_path, "w+")
   file:write(table.concat(respbody))
   file:close()
@@ -125,7 +121,8 @@ end
 
 -- DEPRECATED!!!!!
 function string.starts(String, Start)
-  print("string.starts(String, Start) is DEPRECATED use string:starts(text) instead")
+  -- print("string.starts(String, Start) is DEPRECATED use string:starts(text) instead")
+  -- uncomment if needed
   return Start == string.sub(String,1,string.len(Start))
 end
 
@@ -149,19 +146,6 @@ function unescape_html(str)
   end)
   return new
 end
-function pairsByKeys (t, f)
-    local a = {}
-    for n in pairs(t) do table.insert(a, n) end
-    table.sort(a, f)
-    local i = 0      -- iterator variable
-    local iter = function ()   -- iterator function
-      i = i + 1
-		if a[i] == nil then return nil
-		else return a[i], t[a[i]]
-		end
-	end
-	return iter
-end
 
 function scandir(directory)
   local i, t, popen = 0, {}, io.popen
@@ -183,90 +167,47 @@ function plugins_names( )
   return files
 end
 
--- Function name explains what it does.
-function file_exists(name)
-  local f = io.open(name,"r")
-  if f ~= nil then
-    io.close(f)
-    return true
-  else
-    return false
-  end
-end
-
-function gp_type(chat_id)
-  local gp_type = "pv"
-  local id = tostring(chat_id)
-    if id:match("^-100") then
-      gp_type = "channel"
-    elseif id:match("-") then
-      gp_type = "chat"
-  end
-  return gp_type
-end
-
-function is_reply(msg)
-  local var = false
-    if msg.reply_to_message_id_ ~= 0 then -- reply message id is not 0
-      var = true
-    end
-  return var
-end
-
-function is_supergroup(msg)
-  chat_id = tostring(msg.to.id)
-  if chat_id:match('^-100') then --supergroups and channels start with -100
-    if not msg.is_post_ then
-    return true
-    end
-  else
-    return false
-  end
-end
-
-function is_channel(msg)
-  chat_id = tostring(msg.to.id)
-  if chat_id:match('^-100') then -- Start with -100 (like channels and supergroups)
-  if msg.is_post_ then -- message is a channel post
-    return true
-  else
-    return false
-  end
-  end
-end
-
-function is_group(msg)
-  chat_id = tostring(msg.to.id)
-  if chat_id:match('^-100') then --not start with -100 (normal groups does not have -100 in first)
-    return false
-  elseif chat_id:match('^-') then
-    return true
-  else
-    return false
-  end
-end
-
-function is_private(msg)
-  chat_id = tostring(msg.to.id)
-  if chat_id:match('^-') then --private chat does not start with -
-    return false
-  else
-    return true
-  end
+function pairsByKeys (t, f)
+    local a = {}
+    for n in pairs(t) do table.insert(a, n) end
+    table.sort(a, f)
+    local i = 0      -- iterator variable
+    local iter = function ()   -- iterator function
+      i = i + 1
+		if a[i] == nil then return nil
+		else return a[i], t[a[i]]
+		end
+	end
+	return iter
 end
 
 function check_markdown(text) --markdown escape ( when you need to escape markdown , use it like : check_markdown('your text')
 		str = text
 		if str:match('_') then
-			output = str:gsub('_','\\_')
+			output = str:gsub('_',[[\_]])
 		elseif str:match('*') then
 			output = str:gsub('*','\\*')
 		elseif str:match('`') then
 			output = str:gsub('`','\\`')
+			
 		else
 			output = str
 		end
 	return output
+end
+
+function escape_markdown(name) --markdown escape ( only use it for name of users or groups , use it like : escape_markdown(msg.from.first_name)
+  str = name
+  if str:match('_') then
+   str = str:gsub('_','')
+  end
+	if str:match('*') then
+   str = str:gsub('*','')
+  end
+	if str:match('`') then
+   str = str:gsub('`','')
+  end
+ return str
 end
 
 function is_sudo(msg)
@@ -284,9 +225,9 @@ function is_owner(msg)
   local var = false
   local data = load_data(_config.moderation.data)
   local user = msg.from.id
-  if data[tostring(msg.to.id)] then
-    if data[tostring(msg.to.id)]['owners'] then
-      if data[tostring(msg.to.id)]['owners'][tostring(msg.from.id)] then
+  if data[tostring(msg.chat.id)] then
+    if data[tostring(msg.chat.id)]['owners'] then
+      if data[tostring(msg.chat.id)]['owners'][tostring(msg.from.id)] then
         var = true
       end
     end
@@ -328,17 +269,17 @@ function is_mod(msg)
   local var = false
   local data = load_data(_config.moderation.data)
   local usert = msg.from.id
-  if data[tostring(msg.to.id)] then
-    if data[tostring(msg.to.id)]['mods'] then
-      if data[tostring(msg.to.id)]['mods'][tostring(msg.from.id)] then
+  if data[tostring(msg.chat.id)] then
+    if data[tostring(msg.chat.id)]['mods'] then
+      if data[tostring(msg.chat.id)]['mods'][tostring(msg.from.id)] then
         var = true
       end
     end
   end
 
-  if data[tostring(msg.to.id)] then
-    if data[tostring(msg.to.id)]['owners'] then
-      if data[tostring(msg.to.id)]['owners'][tostring(msg.from.id)] then
+  if data[tostring(msg.chat.id)] then
+    if data[tostring(msg.chat.id)]['owners'] then
+      if data[tostring(msg.chat.id)]['owners'][tostring(msg.from.id)] then
         var = true
       end
     end
@@ -447,7 +388,19 @@ end
   return var
 end
 
- function is_banned(user_id, chat_id)
+function is_filter(msg, text)
+local var = false
+local data = load_data(_config.moderation.data)
+  if data[tostring(msg.chat.id)]['filterlist'] then
+for k,v in pairs(data[tostring(msg.chat.id)]['filterlist']) do 
+    if string.find(string.lower(text), string.lower(k)) then
+       var = true
+        end
+     end
+  end
+ return var
+end
+function is_banned(user_id, chat_id)
   local var = false
   local data = load_data(_config.moderation.data)
   if data[tostring(chat_id)] then
@@ -473,6 +426,19 @@ end
 return var
 end
 
+function is_whitelist(user_id, chat_id)
+  local var = false
+  local data = load_data(_config.moderation.data)
+  if data[tostring(chat_id)] then
+    if data[tostring(chat_id)]['whitelist'] then
+      if data[tostring(chat_id)]['whitelist'][tostring(user_id)] then
+        var = true
+      end
+    end
+  end
+return var
+end
+
 function is_gbanned(user_id)
   local var = false
   local data = load_data(_config.moderation.data)
@@ -486,56 +452,79 @@ function is_gbanned(user_id)
 return var
 end
 
-function is_filter(msg, text)
-local var = false
-local data = load_data(_config.moderation.data)
-  if data[tostring(msg.to.id)]['filterlist'] then
-for k,v in pairs(data[tostring(msg.to.id)]['filterlist']) do 
-    if string.find(string.lower(text), string.lower(k)) then
-       var = true
-        end
-     end
+function ban_user(user_name, user_id, chat_id)
+    local data = load_data(_config.moderation.data)
+if data[tostring(chat_id)]['banned'][tostring(user_id)] then
+     return
+   end
+data[tostring(chat_id)]['banned'][tostring(user_id)] = user_name
+    save_data(_config.moderation.data, data)
+ end
+
+function silent_user(user_name, user_id, chat_id)
+    local data = load_data(_config.moderation.data)
+if data[tostring(chat_id)]['is_silent_users'][tostring(user_id)] then
+     return
+   end
+data[tostring(chat_id)]['is_silent_users'][tostring(user_id)] = user_name
+    save_data(_config.moderation.data, data)
+ end
+
+ function unban_user(user_id, chat_id)
+    local data = load_data(_config.moderation.data)
+if not data[tostring(chat_id)]['banned'][tostring(user_id)] then
+    return
+   end
+data[tostring(chat_id)]['banned'][tostring(user_id)] = nil
+    save_data(_config.moderation.data, data)
   end
- return var
-end
 
-function kick_user(user_id, chat_id)
-if not tonumber(user_id) then
-return false
-end
-  tdcli.changeChatMemberStatus(chat_id, user_id, 'Kicked', dl_cb, nil)
-end
+function unsilent_user(user_id, chat_id)
+    local data = load_data(_config.moderation.data)
+if not data[tostring(chat_id)]['is_silent_users'][tostring(user_id)] then
+    return
+   end
+data[tostring(chat_id)]['is_silent_users'][tostring(user_id)] = nil
+    save_data(_config.moderation.data, data)
+  end
 
-function del_msg(chat_id, message_ids)
-local msgid = {[0] = message_ids}
-  tdcli.deleteMessages(chat_id, msgid, dl_cb, nil)
-end
+function banall_user(user_name, user_id)
+    local data = load_data(_config.moderation.data)
+  if not data['gban_users'] then
+    data['gban_users'] = {}
+    save_data(_config.moderation.data, data)
+    end
+if is_gbanned(user_id) then
+     return
+   end
+  data['gban_users'][tostring(user_id)] = user_name
+    save_data(_config.moderation.data, data)
+  end
+
+function unbanall_user(user_id)
+    local data = load_data(_config.moderation.data)
+  if not data['gban_users'] then
+    data['gban_users'] = {}
+    save_data(_config.moderation.data, data)
+    end
+if not is_gbanned(user_id) then
+     return
+   end
+  data['gban_users'][tostring(user_id)] = nil
+    save_data(_config.moderation.data, data)
+  end
 
  function banned_list(chat_id)
-local hash = "gp_lang:"..chat_id
-local lang = redis:get(hash)
     local data = load_data(_config.moderation.data)
     local i = 1
-  if not data[tostring(msg.chat_id_)] then
-  if not lang then
-    return '_Group is not added_'
-else
-    return '*المجموعه ليست مضافه*'
-   end
+  if not data[tostring(chat_id)] then
+    return '_المجموعه ليست مضافه ‼️😐_'
   end
   -- determine if table is empty
   if next(data[tostring(chat_id)]['banned']) == nil then --fix way
-     if not lang then
-					return "_No_ *banned* _users in this group_"
-   else
-					return "*💡 لايوجد أعضاء محطورين في هذه المجموعه*"
-              end
+					return "لا يوجد مستخدمين محضورين  ‼️😐"
 				end
-       if not lang then
-   message = '*List of banned users :*\n'
-         else
-   message = '_💡 قائمه الاعضاء المحظورين :_\n'
-     end
+   message = '*قائمة المحضورين 🚫 :*\n'
   for k,v in pairs(data[tostring(chat_id)]['banned']) do
     message = message ..i.. '- '..v..' [' ..k.. '] \n'
    i = i + 1
@@ -544,30 +533,16 @@ end
 end
 
  function silent_users_list(chat_id)
-local hash = "gp_lang:"..chat_id
-local lang = redis:get(hash)
     local data = load_data(_config.moderation.data)
     local i = 1
-  if not data[tostring(msg.chat_id_)] then
-  if not lang then
-    return '_Group is not added_'
-else
-    return '*المجموعه ليست مضافه*'
-   end
+  if not data[tostring(chat_id)] then
+    return '_المجموعه ليست مضافه ‼️😐_'
   end
   -- determine if table is empty
   if next(data[tostring(chat_id)]['is_silent_users']) == nil then --fix way
-        if not lang then
-					return "_No_ *silent* _users in this group_"
-   else
-					return "*💡 لايوجد لأعضاء مكتومين في هذه المجموعه*"
-             end
+					return "_لا يوجد مستخدمين مكتومين 🚹_"
 				end
-      if not lang then
    message = '*List of silent users :*\n'
-       else
-   message = '_💡 قائمه الاعضاء المكتومين :_\n'
-    end
   for k,v in pairs(data[tostring(chat_id)]['is_silent_users']) do
     message = message ..i.. '- '..v..' [' ..k.. '] \n'
    i = i + 1
@@ -575,9 +550,29 @@ end
   return message
 end
 
+function whitelist(chat_id)
+    local data = load_data(_config.moderation.data)
+    local i = 1
+  if not data[tostring(chat_id)] then
+    return '_المجموعه ليست مضافه ‼️😐_'
+  end
+  if not data[tostring(chat_id)]['whitelist'] then
+    data[tostring(chat_id)]['whitelist'] = {}
+    save_data(_config.moderation.data, data)
+    end
+  -- determine if table is empty
+  if next(data[tostring(chat_id)]['whitelist']) == nil then --fix way
+					return "_لا يوجد مستخدمين مميزين 🚹_"
+				end
+   message = '*قائمة المميزين البيضاء 🚹💎 :*\n'
+  for k,v in pairs(data[tostring(chat_id)]['whitelist']) do
+    message = message ..i.. '- '..v..' [' ..k.. '] \n'
+   i = i + 1
+end
+  return message
+end
+
  function gbanned_list(msg)
-local hash = "gp_lang:"..msg.chat_id_
-local lang = redis:get(hash)
     local data = load_data(_config.moderation.data)
     local i = 1
   if not data['gban_users'] then
@@ -585,17 +580,9 @@ local lang = redis:get(hash)
     save_data(_config.moderation.data, data)
   end
   if next(data['gban_users']) == nil then --fix way
-    if not lang then
-					return "_No_ *globally banned* _users available_"
-   else
-					return "*💡 لايوجد اعضاء محظورين عام*"
-             end
+					return "لا يوجد مستخدمين محضورين عام ‼️😐"
 				end
-        if not lang then
    message = '*List of globally banned users :*\n'
-   else
-   message = '_💡 قائمه المحظورين عام :_\n'
-   end
   for k,v in pairs(data['gban_users']) do
     message = message ..i.. '- '..v..' [' ..k.. '] \n'
    i = i + 1
@@ -604,41 +591,126 @@ end
 end
 
  function filter_list(msg)
-local hash = "gp_lang:"..msg.chat_id_
-local lang = redis:get(hash)
     local data = load_data(_config.moderation.data)
-  if not data[tostring(msg.chat_id_)]['filterlist'] then
-    data[tostring(msg.chat_id_)]['filterlist'] = {}
+  if not data[tostring(msg.chat.id)]['filterlist'] then
+    data[tostring(msg.chat.id)]['filterlist'] = {}
     save_data(_config.moderation.data, data)
     end
-  if not data[tostring(msg.chat_id_)] then
-  if not lang then
-    return '_Group is not added_'
-else
-    return '*المجموعة ليست مضافة*'
-   end
+  if not data[tostring(msg.chat.id)] then
+    return '_المجموعه ليست مضافه ‼️😐_'
   end
   -- determine if table is empty
-  if next(data[tostring(msg.chat_id_)]['filterlist']) == nil then --fix way
-      if not lang then
-    return "*Filtered words list* _is empty_"
-      else
-    return "_💡 قائمه الكلمات الممنوعه فارغه_"
-     end
+  if next(data[tostring(msg.chat.id)]['filterlist']) == nil then --fix way
+    return "قائمة الكلمات الممنوعه خاليه 💎"
   end
-  if not data[tostring(msg.chat_id_)]['filterlist'] then
-    data[tostring(msg.chat_id_)]['filterlist'] = {}
+  if not data[tostring(msg.chat.id)]['filterlist'] then
+    data[tostring(msg.chat.id)]['filterlist'] = {}
     save_data(_config.moderation.data, data)
     end
-      if not lang then
-       filterlist = '*List of filtered words :*\n'
-         else
-       filterlist = '_💡 قائمه الكلمات الممنوعه :_\n'
-    end
+       filterlist = '*قائمة الكلمات الممنوعه  💎 :*\n'
  local i = 1
-   for k,v in pairs(data[tostring(msg.chat_id_)]['filterlist']) do
-              filterlist = filterlist..'*'..i..'* - _'..k..'_\n'
+   for k,v in pairs(data[tostring(msg.chat.id)]['filterlist']) do
+              filterlist = filterlist..'*'..i..'* - _'..check_markdown(k)..'_\n'
              i = i + 1
          end
      return filterlist
    end
+
+function get_var_inline(msg)
+if msg.query then
+if msg.query:match("-%d+") then
+msg.chat = {}
+msg.chat.id = "-"..msg.query:match("%d+")
+    end
+elseif not msg.query then
+msg.chat.id = msg.chat.id
+end
+match_plugins(msg)
+end
+function get_var(msg)
+ msg.data = {}
+msg.to = {}
+msg.id = msg.message_id
+if msg.chat.type ~= "private" then
+msg.to.id = msg.chat.id
+msg.to.type = msg.chat.type
+msg.to.title = msg.chat.title
+  else
+msg.to.id = msg.chat.id
+msg.to.type = msg.chat.type
+msg.to.title = false
+end
+if msg.game or msg.new_chat_member or msg.left_chat_member or msg.new_chat_title or msg.new_chat_photo or msg.delete_chat_photo or msg.pinned_message then
+  msg.service = true
+   else
+  msg.service = false
+ end
+ if msg.left_chat_member then
+msg.deluser = {}
+ msg.deluser.id = msg.left_chat_member.id
+if msg.left_chat_member.last_name then
+msg.deluser.print_name = msg.left_chat_member.first_name..' '..msg.left_chat_member.last_name
+  else
+msg.deluser.print_name = msg.left_chat_member.first_name
+end
+msg.deluser.username = msg.left_chat_member.username
+msg.deluser.first_name = msg.left_chat_member.first_name
+msg.deluser.last_name = msg.left_chat_member.last_name
+ end
+ if msg.new_chat_member then
+msg.newuser = {}
+ msg.newuser.id = msg.new_chat_member.id
+if msg.new_chat_member.last_name then
+msg.newuser.print_name = msg.new_chat_member.first_name..' '..msg.new_chat_member.last_name
+  else
+msg.newuser.print_name = msg.new_chat_member.first_name
+end
+msg.newuser.username = msg.new_chat_member.username
+msg.newuser.first_name = msg.new_chat_member.first_name
+msg.newuser.last_name = msg.new_chat_member.last_name
+ end
+if msg.reply_to_message then
+msg.reply = {}
+msg.reply_id = msg.reply_to_message.message_id
+msg.reply.id = msg.reply_to_message.from.id
+if msg.reply_to_message.from.last_name then
+msg.reply.print_name = msg.reply_to_message.from.first_name..' '..msg.reply_to_message.from.last_name
+else
+msg.reply.print_name = msg.reply_to_message.from.first_name
+end
+msg.reply.first_name = msg.reply_to_message.from.first_name
+msg.reply.last_name = msg.reply_to_message.from.last_name
+msg.reply.username = msg.reply_to_message.from.username
+if msg.reply_to_message.forward_from then
+msg.reply.fwd_from = {}
+msg.reply.fwd_from.id = msg.reply_to_message.forward_from.id
+if msg.reply_to_message.forward_from.last_name then
+msg.reply.fwd_from.print_name = msg.reply_to_message.forward_from.first_name..' '..msg.reply_to_message.forward_from.last_name
+else
+msg.reply.fwd_from.print_name = msg.reply_to_message.forward_from.first_name
+end
+msg.reply.fwd_from.first_name = msg.reply_to_message.forward_from.first_name
+msg.reply.fwd_from.last_name = msg.reply_to_message.forward_from.last_name
+msg.reply.fwd_from.username = msg.reply_to_message.forward_from.username
+  end
+end
+if msg.from.last_name then
+msg.from.print_name = msg.from.first_name..' '..msg.from.last_name
+else
+msg.from.print_name = msg.from.first_name
+end
+if msg.forward_from then
+msg.fwd_from = {}
+msg.fwd_from.id = msg.forward_from.id
+msg.fwd_from.first_name = msg.forward_from.first_name
+msg.fwd_from.last_name = msg.forward_from.last_name
+if msg.forward_from.last_name then
+msg.fwd_from.print_name = msg.forward_from.first_name..' '..msg.forward_from.last_name
+else
+msg.fwd_from.print_name = msg.forward_from.first_name
+end
+msg.fwd_from.username = msg.forward_from.username
+end
+match_plugins(msg)
+end
+
